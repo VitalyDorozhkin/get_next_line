812e4ff328ecac7a009f1f4749c1cfc46cbec086
Pibbles Paralax <pparalax@mi-q6.21-school.ru>
2019-09-20 15:46:53 +0300

    1

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pparalax <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/16 14:09:18 by pparalax          #+#    #+#             */
+/*   Updated: 2019/09/16 14:09:20 by pparalax         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <fcntl.h>
+
+int		get_next_line(const int fd, char **line);
+#endif
